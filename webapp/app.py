dc525d9fb58d79c164731fd434627df558974186
wxpq <ampmp49@gmail.com>
2021-02-20 13:13:34 +0330

    dsadasda

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,8 @@ from urllib.request import pathname2url, url2pathname
 import os, glob, sys
 
 app = Flask("Flask Upload Server")
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 3000
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -11,7 +13,7 @@ def home():
         return docu()
     file = request.files["file"]
     file.save(os.path.join("cdn", file.filename))
-    return "wget https://personal-mini-cloud.herokuapp.com/cdn/{}\n\n".format(pathname2url(file.filename))
+    return "wget https://jack1100up.herokuapp.com/cdn/{}\n\n".format(pathname2url(file.filename))
 
 @app.route('/cdn/<path:codeword>')
 def download_file(codeword):
@@ -22,7 +24,7 @@ def download_file(codeword):
 def get_list():
     return "<br>".join([
     "<b><u>All uploads</u></b>",
-    *["wget <a href='https://personal-mini-cloud.herokuapp.com/cdn/{0}'>https://personal-mini-cloud.herokuapp.com/cdn/{0}</a>\n\n".format(pathname2url(file)) for file in os.listdir("cdn")]
+    *["wget <a href='https://jack1100up.herokuapp.com/cdn/{0}'>https://jack1100up.herokuapp.com/cdn/{0}</a>\n\n".format(pathname2url(file)) for file in os.listdir("cdn")]
     ])
 
 def docu():
@@ -30,13 +32,12 @@ def docu():
     "Welcome to Personal Mini Cloud.",
     "========================================================================================",
     "Usage:",
-    "curl -i -X POST -F file=@FILE_PATH  https://personal-mini-cloud.herokuapp.com",
+    "curl -F file=@FILE_PATH  https://jack1100up.herokuapp.com",
     "",
     "Replace FILE_PATH with absolute path of the file to be uploaded to the cloud."
     "","",
-    "<a href='https://personal-mini-cloud.herokuapp.com/all'>List all uploads</a>",
-    "",
-    "Star/fork/improve my source code: <a href='https://github.com/piyush-kgp/File-upload-server'>https://github.com/piyush-kgp/File-upload-server</a>"
+    "<a href='https://jack1100up.herokuapp.com/all'>List all uploads</a>",
+    ""
     ])
 
 if __name__=="__main__":
