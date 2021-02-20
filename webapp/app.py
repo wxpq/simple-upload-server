bcf1099ecb921338ff48c30bdb13c65f0cff6066
wxpq <ampmp49@gmail.com>
2021-02-20 17:02:40 +0330

    dsadasda

--- webapp/app.py
+++ webapp/app.py
@@ -22,8 +22,8 @@ def uploadfile(filename):
         chunk_size = 1024
         while bytes_left > 0:
             if bytes_left == int(request.headers.get('content-length')):
-                garbage = request.stream.read(512).decode().split('\r\n')[0]
-                garbage_str = str(garbage)
+                garbage = str(request.stream.read(512),'UTF-8').split('\r\n')[0]
+                garbage_str = garbage
                 bytes_left -= len(garbage_str)
             else:
                 chunk = request.stream.read(chunk_size)
