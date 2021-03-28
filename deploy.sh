5699601b83a3b8590dbe963229c518b49567a96f
Ali Gh <45707069+wxpq@users.noreply.github.com>
2021-03-28 06:10:55 +0430

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
-
-# heroku login -i
-# heroku container:login
-
-# heroku create personal-mini-cloud
 heroku container:push web --app personal-mini-cloud
 heroku container:release web --app personal-mini-cloud
 heroku logs --app personal-mini-cloud --tail
